19a389e702569911247b126ac95d403a81f6659f
Giovanni Bassi <giggio@giggio.net>
2020-12-02 17:28:34 -0300

    First

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.radList = new Telerik.WinControls.UI.RadDropDownList();
+            ((System.ComponentModel.ISupportInitialize)(this.radList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.CreationProperties = null;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webView21.Location = new System.Drawing.Point(0, 153);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(800, 1026);
+            this.webView21.TabIndex = 0;
+            this.webView21.Text = "webView21";
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // radList
+            // 
+            this.radList.Location = new System.Drawing.Point(276, 57);
+            this.radList.Name = "radList";
+            this.radList.Size = new System.Drawing.Size(500, 35);
+            this.radList.TabIndex = 1;
+            this.radList.Text = "radDropDownList1";
+            this.radList.SelectedIndexChanged += new Telerik.WinControls.UI.Data.PositionChangedEventHandler(this.radList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 1179);
+            this.Controls.Add(this.radList);
+            this.Controls.Add(this.webView21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.radList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private Telerik.WinControls.UI.RadDropDownList radList;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webView21.Source = new("https://mobile.twitter.com/giovannibassi");
+            List<Pessoa> pessoas = new()
+            {
+                new("Giovanni", "https://mobile.twitter.com/giovannibassi"),
+                new("VC", "https://mobile.twitter.com/victorhg"),
+                new("VH", "https://mobile.twitter.com/vcavalcante"),
+            };
+            radList.DataSource = pessoas;
+            radList.DisplayMember = nameof(Pessoa.Nome);
+            radList.Font = new("Consolas", 14);
+            radList.ListElement.Font = radList.Font;
+            radList.ListElement.Font = new("Consolas", 24);
+            radList.ListElement.AutoSizeItems = true;
+        }
+
+
+        private void radList_SelectedIndexChanged(object sender, Telerik.WinControls.UI.Data.PositionChangedEventArgs e) =>
+            webView21.Source = new(((Pessoa)radList.SelectedItem.DataBoundItem).Url);
+    }
+    record Pessoa(string Nome, string Url);
+}
